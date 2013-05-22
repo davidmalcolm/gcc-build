ebb019d3b4df713ffa1df47b8d00f4a0907aa962
David Malcolm <dmalcolm@redhat.com>
2013-05-22 17:03:17 -0400

    Initial commit

---
+++ Makefile
@@ -0,0 +1,74 @@
+DEPDIR=$(PWD)/dep-prefix
+
+ # this bundles isl-0.11.1:
+CLOOG_VERSION=0.18.0
+
+GMP_VERSION=4.3.2
+
+MPC_VERSION=0.8.1
+
+MPFR_VERSION=2.4.2
+
+all: systemdeps deps
+
+systemdeps:
+	yum install -y \
+	  dejagnu
+	  texinfo
+
+deps: \
+	s-cloog \
+	s-gmp \
+	s-mpc \
+	s-mpfr
+
+s-cloog: cloog-$(CLOOG_VERSION).tar.gz
+	tar -zxf $<
+	(cd cloog-$(CLOOG_VERSION) \
+	  && ./configure --prefix=$(DEPDIR) \
+	  && make \
+	  && make install \
+	) && touch $@
+
+s-gmp: gmp-$(GMP_VERSION).tar.bz2
+	tar -jxf $<
+	(cd gmp-$(GMP_VERSION) \
+	  && ./configure --prefix=$(DEPDIR) \
+	  && make \
+	  && make install \
+	) && touch $@
+
+s-mpc: mpc-$(MPC_VERSION).tar.gz
+	tar -zxf $<
+	(cd mpc-$(MPC_VERSION) \
+	  && ./configure --prefix=$(DEPDIR) \
+	  && make \
+	  && make install \
+	) && touch $@
+
+s-mpfr: mpfr-$(MPFR_VERSION).tar.bz2
+	tar -jxf $<
+	(cd mpfr-$(MPFR_VERSION) \
+	  && ./configure --prefix=$(DEPDIR) \
+	  && make \
+	  && make install \
+	) && touch $@
+
+dep-tarballs:  \
+	cloog-$(CLOOG_VERSION).tar.gz \
+	gmp-$(GMP_VERSION).tar.bz2 \
+	mpc-$(MPC_VERSION).tar.gz \
+	mpfr-$(MPFR_VERSION).tar.bz2 
+
+cloog-$(CLOOG_VERSION).tar.gz:
+	wget ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-$(CLOOG_VERSION).tar.gz
+
+gmp-$(GMP_VERSION).tar.bz2:
+	wget ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-$(GMP_VERSION).tar.bz2
+
+mpc-$(MPC_VERSION).tar.gz:
+	wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-$(MPC_VERSION).tar.gz
+
+mpfr-$(MPFR_VERSION).tar.bz2:
+	wget ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-$(MPFR_VERSION).tar.bz2
+
---
+++ build.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+DEPDIR=$(pwd)/dep-prefix
+echo DEPDIR=$DEPDIR
+
+TESTDIR=$(pwd)/test
+CONTROL=$TESTDIR/control
+EXPERIMENT=$TESTDIR/experiment
+#rm -rf $TESTDIR
+mkdir $TESTDIR
+mkdir $CONTROL
+mkdir $EXPERIMENT
+
+make deps
+
+J=-j16
+
+create_src()
+{
+    BASEDIR=$1
+    echo "create_src $BASEDIR"
+    rm -rf $BASEDIR/src
+    cp -a gcc $BASEDIR/src
+}
+
+create_build()
+{
+    BASEDIR=$1
+    echo "create_build $BASEDIR"
+    rm -rf $BASEDIR/build
+    mkdir $BASEDIR/build
+    rm -rf $BASEDIR/install
+    mkdir $BASEDIR/install
+    (cd $BASEDIR/build \
+     && ../src/configure \
+          --prefix=$BASEDIR/install \
+	  --with-mpc=$DEPDIR \
+	  --with-mpfr=$DEPDIR \
+	  --with-gmp=$DEPDIR \
+	  --with-cloog=$DEPDIR
+    ) || exit 1
+}
+
+# would have --with-isl=$DEPDIR
+# but conmfigure fails; the embedded isl in cloog seems to return
+# isl_version() of "UNKNOWN"
+
+# failed with:
+#    /root/test/control/build/./gcc/cc1: error while loading shared libraries: libmpc.so.2: cannot open shared object file: No such file or directory
+
+
+invoke_build()
+{
+    BASEDIR=$1
+    echo "invoke_build $BASEDIR"
+    cd $BASEDIR/build
+
+    export LD_LIBRARY_PATH=$DEPDIR/lib:$LD_LIBRARY_PATH
+
+    # Build:
+    /usr/bin/time make $J || exit 1
+
+    # Test suite:
+    /usr/bin/time make check $J || exit 1
+}
+
+create_src $CONTROL
+create_src $EXPERIMENT
+
+# Apply patches:
+for p in *.patch ; do
+    OLD_PWD=$(pwd)
+    (cd $EXPERIMENT/src && git apply $OLD_PWD/$p)
+done
+
+create_build $CONTROL
+create_build $EXPERIMENT
+
+invoke_build $CONTROL
+invoke_build $EXPERIMENT
+
+# Comapre test results:
+$CONTROL/src/contrib/compare_tests \
+  $CONTROL/build/gcc/testsuite \
+  $EXPERIMENT/build/gcc/testsuite
