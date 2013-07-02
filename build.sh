d449092f3b9ca03ebb3292c77da58249eeba80e8
David Malcolm <dmalcolm@redhat.com>
2013-07-02 16:40:50 -0400

    record what patches were applied by committing them to experiment's git repo

--- build.sh
+++ build.sh
@@ -83,9 +83,14 @@ create_src $CONTROL
 create_src $EXPERIMENT
 
 # Apply patches:
-for p in *.patch ; do
+for p in $(echo *.patch | sort); do
     OLD_PWD=$(pwd)
-    (cd $EXPERIMENT/src && git apply $OLD_PWD/$p) || exit 1
+    echo "Applying patch $p"
+    # "git apply --index" doesn't seem to work
+    (cd $EXPERIMENT/src \
+     && git apply $OLD_PWD/$p \
+     && git add $(diffstat -lp1 $OLD_PWD/$p) \
+     && git commit -m"Experimental patch: $p") || exit
 done
 
 create_build $CONTROL
