0b3627a87843016f7c229ad49416ba20e93c6be5
David Malcolm <dmalcolm@redhat.com>
2013-08-31 07:12:20 -0400

    compare all .sum files, not just those under gcc

--- compare-builds.sh
+++ compare-builds.sh
@@ -4,5 +4,32 @@ source support.sh
 
 # Compare test results:
 $CONTROL/src/contrib/compare_tests \
-  $CONTROL/build/gcc/testsuite \
-  $EXPERIMENT/build/gcc/testsuite
+  $CONTROL/build \
+  $EXPERIMENT/build
+
+# For reference here are the 2*12=24 sum files that should be found:
+#  $ find test/*/build -name "*.sum"
+#  test/control/build/gcc/testsuite/gfortran/gfortran.sum
+#  test/control/build/gcc/testsuite/gcc/gcc.sum
+#  test/control/build/gcc/testsuite/objc/objc.sum
+#  test/control/build/gcc/testsuite/g++/g++.sum
+#  test/control/build/x86_64-unknown-linux-gnu/boehm-gc/testsuite/boehm-gc.sum
+#  test/control/build/x86_64-unknown-linux-gnu/libstdc++-v3/testsuite/libstdc++.sum
+#  test/control/build/x86_64-unknown-linux-gnu/libmudflap/testsuite/libmudflap.sum
+#  test/control/build/x86_64-unknown-linux-gnu/libitm/testsuite/libitm.sum
+#  test/control/build/x86_64-unknown-linux-gnu/libatomic/testsuite/libatomic.sum
+#  test/control/build/x86_64-unknown-linux-gnu/libjava/testsuite/libjava.sum
+#  test/control/build/x86_64-unknown-linux-gnu/libffi/testsuite/libffi.sum
+#  test/control/build/x86_64-unknown-linux-gnu/libgomp/testsuite/libgomp.sum
+#  test/experiment/build/gcc/testsuite/gfortran/gfortran.sum
+#  test/experiment/build/gcc/testsuite/gcc/gcc.sum
+#  test/experiment/build/gcc/testsuite/objc/objc.sum
+#  test/experiment/build/gcc/testsuite/g++/g++.sum
+#  test/experiment/build/x86_64-unknown-linux-gnu/boehm-gc/testsuite/boehm-gc.sum
+#  test/experiment/build/x86_64-unknown-linux-gnu/libstdc++-v3/testsuite/libstdc++.sum
+#  test/experiment/build/x86_64-unknown-linux-gnu/libmudflap/testsuite/libmudflap.sum
+#  test/experiment/build/x86_64-unknown-linux-gnu/libitm/testsuite/libitm.sum
+#  test/experiment/build/x86_64-unknown-linux-gnu/libatomic/testsuite/libatomic.sum
+#  test/experiment/build/x86_64-unknown-linux-gnu/libjava/testsuite/libjava.sum
+#  test/experiment/build/x86_64-unknown-linux-gnu/libffi/testsuite/libffi.sum
+#  test/experiment/build/x86_64-unknown-linux-gnu/libgomp/testsuite/libgomp.sum
