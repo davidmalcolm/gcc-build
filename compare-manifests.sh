0268aec66a3498b78100302e703e010e8fb86b88
David Malcolm <dmalcolm@redhat.com>
2014-04-15 11:15:21 -0400

    Update compare-manifests.sh for change to support alternate targets

--- compare-manifests.sh
+++ compare-manifests.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+source support.sh
+
+echo $CONFIG
+
+get_manifest()
+{
+    DIR=$1
+    FILE=$2
+
+    (cd $DIR \
+	&& (find \
+	| grep -v ".git/" \
+	| sort)
+    ) > $2
+}
+
 for what in control experiment ; do
-    for stage in src build install ; do
-	(cd test/$what/$stage \
-	    && (find \
-		| grep -v ".git/" \
-		| sort)
-	) > manifest-of-$what-$stage.txt
-    done
+    get_manifest test/$what/src manifest-of-$what-src.txt
+    get_manifest test/$what/$CONFIG/build manifest-of-$what-$CONFIG-build.txt
+    get_manifest test/$what/$CONFIG/install manifest-of-$what-$CONFIG-install.txt
 done
 
-for stage in src build install ; do
+for stage in src $CONFIG-build $CONFIG-install ; do
   diff -up \
     manifest-of-control-$stage.txt \
     manifest-of-experiment-$stage.txt \
