165d1f1d0622a0a21c4ac369e61ce8b997ff0faf
David Malcolm <dmalcolm@redhat.com>
2020-02-14 13:30:08 -0500

    Add phases to benchmarking

--- benchmark.py
+++ benchmark.py
@@ -11,17 +11,42 @@ class Stats(namedtuple('Stats', STAT_FIELDS)):
 
 class GccLog:
     def __init__(self, path):
+        self.phases = {}
         with open(path) as f:
             for line in f:
                 ws = r'\s+'
+
+                # Parse "TOTAL" line:
                 timing = r'([0-9]+.[0-9]+)'
                 m = re.match(r'^ TOTAL' + ws + ':' + 3 * (ws + timing) + ws + r'([0-9]+) kB$', line)
                 if m:
-                    self.stats = Stats(*[float(f) for f in m.groups()])
+                    print(m.groups())
+                    self.phases['total'] = Stats(*[float(f) for f in m.groups()])
 
-TESTS = ['kdecore.cc', 'big-code.c']
+                timing = ws + timing + ws + r'\(.*\)' + ws
+
+                # Parse "phase " lines:
+                m = re.match(r'^ (phase .*)' + ws + ':'
+                             + timing #+ 'usr'
+                             + timing #+ 'sys'
+                             + timing #+ 'wall'
+                             + ws + r'([0-9]+) kB',
+                             line)
+                if m:
+                    print(m.groups())
+                    self.phases[m.group(1).strip()] = Stats(*[float(f) for f in m.groups()[1:]])
+
+TESTS = ['cp-stdlib.cc', 'kdecore.cc']
 GROUPS = ['control', 'experiment']
-CONFIGS = ['x86_64-unknown-linux-gnu']
+CONFIGS = ['x86_64-pc-linux-gnu']
+PHASES = ['total',
+          'phase setup',
+          'phase parsing',
+          'phase lang. deferred',
+          #'phase opt and generate',
+          #'phase last asm'
+]
+
 logfiles = {}
 for test in TESTS:
     for group in GROUPS:
@@ -58,20 +83,21 @@ for test in TESTS:
 for test in TESTS:
     for config in CONFIGS:
         for stat_field in STAT_FIELDS:
-            benchmark_name = 'Compilation of %s at -O3 with -g for %s: %s' % (test, config, stat_field)
-            data = {}
-            for group in GROUPS:
-                data[group] = [getattr(log.stats, stat_field)
-                                for log in logs[(test, group, config)]]
-            print(benchmark_name)
-            for group in GROUPS:
-                print('   %10s: %s' % (group, data[group]))
-            if stat_field == 'ggc':
-                print(perf.CompareMemoryUsage(data['control'],
-                                              data['experiment'],
-                                              Options(benchmark_name)))
-            else:
-                print(perf.CompareMultipleRuns(data['control'],
-                                               data['experiment'],
-                                               Options(benchmark_name)))
-            print('')
+            for phase in PHASES:
+                benchmark_name = 'Compilation of %s at -O3 with -g for %s: %s: %s' % (test, config, phase, stat_field)
+                data = {}
+                for group in GROUPS:
+                    data[group] = [getattr(log.phases[phase], stat_field)
+                                    for log in logs[(test, group, config)]]
+                print(benchmark_name)
+                for group in GROUPS:
+                    print('   %10s: %s' % (group, data[group]))
+                if stat_field == 'ggc':
+                    print(perf.CompareMemoryUsage(data['control'],
+                                                  data['experiment'],
+                                                  Options(benchmark_name)))
+                else:
+                    print(perf.CompareMultipleRuns(data['control'],
+                                                   data['experiment'],
+                                                   Options(benchmark_name)))
+                print('')
